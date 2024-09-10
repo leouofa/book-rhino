0fb3c78f6ff37744779166fdbcef4f935057a8d4
Leonid Medovyy <leonid@macron1.io>
2024-09-09 17:16:30 -0700

    removing views

--- app/controllers/imaginations_controller.rb
+++
@@ -1,8 +0,0 @@
-class ImaginationsController < ApplicationController
-  include RestrictedAccess
-
-  def index
-    @imaginations = Imagination.successful.order(id: :desc).page params[:page]
-    @total_imaginations = Imagination.successful.count
-  end
-end
