99007d21bad96d81f0beadbc974ea8cc675c2a1b
Leonid Medovyy <leonid@macron1.io>
2024-09-09 19:37:14 -0700

    fixing tests

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,20 +3,16 @@
 # Table name: settings
 #
 #  id                 :bigint           not null, primary key
-#  topics             :text
 #  prompts            :text
 #  tunings            :text
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  publish_start_time :time             default(Sat, 01 Jan 2000 08:00:00.000000000 UTC +00:00)
 #  publish_end_time   :time             default(Sat, 01 Jan 2000 21:00:00.000000000 UTC +00:00)
-#  pillars            :text
 #
 class Setting < ApplicationRecord
-  serialize :topics
-  serialize :prompts
-  serialize :tunings
-  serialize :pillars
+  serialize :prompts, coder: YAML
+  serialize :tunings, coder: YAML
 
   before_create :set_default_publish_times
 
