bc2b524ea6d45fe76c7173423ab432bea8182bd8
Leonid Medovyy <leonid@macron1.io>
2024-09-15 14:37:24 -0700

    setting up the breadcrumb method

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -2,4 +2,8 @@ module MetaHelper
   def parent_reference(form, parent)
     form.hidden_field("#{parent.class.name.underscore}_id", value: parent.id)
   end
+
+  def breadcrumb_parent(name, path)
+    link_to(name, path, class: 'text-blue-700 hover:text-blue-700') + '&nbsp;/&nbsp;'.html_safe
+  end
 end
