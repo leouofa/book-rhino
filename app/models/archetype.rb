3ff46e295704699e50ecfd50d8fd6058af59ddd8
Leonid Medovyy <leonid@macron1.io>
2024-09-22 12:57:13 -0700

    setting up archetypes

---
+++ app/models/archetype.rb
@@ -0,0 +1,2 @@
+class Archetype < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_183431) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_195043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "vector"
+
+  create_table "archetypes", force: :cascade do |t|
+    t.string "name"
+    t.text "traits"
+    t.text "examples"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.text "title"
