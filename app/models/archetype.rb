41fa6e3250459880a1de0553b4dd986ba8d5648e
Leonid Medovyy <leonid@macron1.io>
2024-10-13 14:24:00 -0700

    setting up hbtm realtionship to characters

--- app/models/archetype.rb
+++ app/models/archetype.rb
@@ -10,7 +10,7 @@
 #  updated_at :datetime         not null
 #
 class Archetype < ApplicationRecord
-  has_any_belongs_to_many :characters
+  has_and_belongs_to_many :characters
 
   paginates_per 100
 end
