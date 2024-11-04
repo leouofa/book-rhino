9589b7b034bd978cc99db0953b66f5bed6f43e80
Leonid Medovyy <leonid@macron1.io>
2024-11-04 15:59:02 -0700

    refactoring the merge job

--- app/jobs/merge_character_prompts_job.rb
+++ app/jobs/merge_character_prompts_job.rb
@@ -1,58 +1,26 @@
-class MergeCharacterPromptsJob < ApplicationJob
-  queue_as :default
-
-  def perform(character, version_to_merge)
-    prompt_1 = character.prompt
-    prompt_2 = version_to_merge.prompt
-
-    @client = OpenAI::Client.new
-
-    system_role = <<~SYSTEM_ROLE
-      You are a college level english teacher.
-      You will be provided with two character descriptions for ChatGPT.
-      Your task is to join the two character descriptions and factor out commonalities, merging them into a cohesive, unified character description.#{' '}
-      Return only the character description.
-      DONT MAKE ANYTHING UP.
-    SYSTEM_ROLE
-
-    messages = [
-      { role: "system", content: system_role },
-      { role: "user", content: "description_1:\n#{prompt_1}\n--------\n#description_2:\n#{prompt_2}" }
-    ]
+class MergeCharacterPromptsJob < MetaJob
+  def perform(component, version_to_merge)
+    @component = component
+    @version_to_merge = version_to_merge
 
     sleep(0.5)
-
-    character.update(pending: true)
-    broadcast_character_update(character)
-
+    super()
     sleep(3)
-
-    response = chat(messages:)
-
-    character.update(prompt: response["choices"][0]["message"]["content"], pending: false)
-
-    broadcast_character_update(character)
   end
 
   private
 
-  def chat(messages:)
-    @client.chat(
-      parameters: {
-        model: ENV['OPENAI_GPT_MODEL'], # Required.
-        messages:,
-        temperature: 0.7
-      }
-    )
+  def system_role
+    <<~SYSTEM_ROLE
+      You are a college level english teacher.
+      You will be provided with two character descriptions for ChatGPT.
+      Your task is to join the two character descriptions and factor out commonalities, merging them into a cohesive, unified character description.
+      Return only the character description.
+      DONT MAKE ANYTHING UP.
+    SYSTEM_ROLE
   end
 
-  def broadcast_character_update(character)
-    # This broadcasts to the specific Turbo Stream channel for the writing style
-    Turbo::StreamsChannel.broadcast_update_to(
-      "character_#{character.id}", # unique identifier for the writing style
-      target: "character_#{character.id}_prompt", # the DOM ID where the prompt will be inserted
-      partial: "characters/prompt", # partial view to render the updated content
-      locals: { character: }
-    )
+  def user_content
+    "description_1:\n#{@component.prompt}\n--------\n#description_2:\n#{@version_to_merge.prompt}"
   end
 end
