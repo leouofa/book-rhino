eb6fccc00046a730149e236473e1407d3c8f0e02
Leonid Medovyy <leonid@macron1.io>
2024-11-04 15:18:27 -0700

    setting up the meta controller

--- app/controllers/character/versions_controller.rb
+++ app/controllers/character/versions_controller.rb
@@ -1,49 +1,15 @@
-class Character::VersionsController < ApplicationController
-  before_action :set_character
-  before_action :set_version, only: %i[revert merge]
-
-  def index
-    @versions_with_prompt_changes = @character.versions.map do |version|
-      changes = parse_object_changes(version)
-      found = changes.any? { |property, (old_value, _new_value)| property == 'prompt' && old_value }
-
-      next unless found
-
-      {
-        id: version.id,
-        updated_at: changes['updated_at']&.last,
-        old_prompt: changes['prompt']&.first
-      }
-    end.compact
-  end
-
-  def revert
-    version_to_revert = @version.reify
-    version_to_revert.pending = false
-    version_to_revert.save
-
-    redirect_to character_path(@character), notice: "Version reverted successfully."
-  end
-
-  def merge
-    version_to_merge = @version.reify
-    MergeCharacterPromptsJob.perform_later(@character, version_to_merge)
-
-    redirect_to character_path(@character)
-  end
-
+class Character::VersionsController < MetaVersionController
   private
 
-  def set_character
-    @character = Character.find_by!(id: params[:character_id])
+  def parent_class
+    Character
   end
 
-  def set_version
-    @version = @character.versions.find_by!(id: params[:id])
+  def merge_job
+    MergeCharacterPromptsJob
   end
 
-  def parse_object_changes(version)
-    permitted_classes = [Time, Date, ActiveSupport::TimeWithZone, ActiveSupport::TimeZone]
-    YAML.safe_load(version.object_changes, permitted_classes:, aliases: true)
+  def redirect_path
+    character_path(@parent)
   end
 end
