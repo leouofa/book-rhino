049c6ceeb4f185dcbd0c506badbbf2d49057fdc6
Leonid Medovyy <leonid@macron1.io>
2024-11-26 14:20:16 -0700

    annoated region

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: regions
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  city        :string
+#  country     :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Region < ApplicationRecord
 end
