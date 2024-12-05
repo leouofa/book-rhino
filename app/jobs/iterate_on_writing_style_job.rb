53b9d2a78108cd1ed8376c46318d1372bbc14cb6
Leonid Medovyy <leonid@macron1.io>
2024-12-05 12:41:09 -0700

    cleaning up the writing style iteration job

--- app/jobs/iterate_on_writing_style_job.rb
+++ app/jobs/iterate_on_writing_style_job.rb
@@ -6,16 +6,14 @@ class IterateOnWritingStyleJob < MetaJob
     @message = message
     @writing_style_json = JSON.parse(component.prompt)
 
-    sleep(0.1)
     super()
-    sleep(3)
   end
 
   private
 
   def system_role
     <<~SYSTEM_ROLE
-      You are a college level english teacher. 
+      You are a college level english teacher.
       You will be provided with a list(containing a set of instructions describing a writing style) and
       a request asking to modify it. Complete the request and return the new list with numbers 1 through n in JSON format
     SYSTEM_ROLE
