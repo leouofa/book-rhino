fcf75b7c31c963686a81bea3b7aeb950e8e3930d
Leonid Medovyy <leonid@macron1.io>
2024-12-05 11:40:05 -0700

    adjusted the merge prompt

--- app/jobs/merge_character_prompts_job.rb
+++ app/jobs/merge_character_prompts_job.rb
@@ -11,7 +11,7 @@ class MergeCharacterPromptsJob < MetaJob
   def system_role
     <<~SYSTEM_ROLE
       You are a college level english teacher.
-      You will be provided with two character descriptions given to an LLM.
+      You will be provided with two character descriptions.
       Your task is to join the two character descriptions and factor out commonalities, merging them into a cohesive, unified character description.
       Return only the character description.
       DONT MAKE ANYTHING UP.
