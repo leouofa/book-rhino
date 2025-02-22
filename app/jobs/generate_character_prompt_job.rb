46f9a38a58c36da8caa142bce99e3a41d2153de3
Leonid Medovyy <leonid@macron1.io>
2025-02-22 11:06:29 -0700

    updated the character prompt

--- app/jobs/generate_character_prompt_job.rb
+++ app/jobs/generate_character_prompt_job.rb
@@ -10,7 +10,7 @@ class GenerateCharacterPromptJob < MetaJob
 
   def system_role
     <<~SYSTEM_ROLE
-      You are a college level english teacher. Analyze the following JSON Object and create a 3 paragraph character description to be given to an LLM. Don't mention JSON object. DONT MAKE ANYTHING UP.
+      You are a college level english teacher. Analyze the following JSON Object and create a 3 paragraph character description to be given to a movie director. Don't mention JSON object. DONT MAKE ANYTHING UP.
     SYSTEM_ROLE
   end
 
