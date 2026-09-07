f6f9ecab55ddfa4ce45166ff9eb1cde38ebe2610
Leonid Medovyy <leonid@macron1.io>
2026-09-06 22:10:31 -0700

    migrate codemirror from v5 to v6

--- app/frontend/controllers/code_controller.js
+++ app/frontend/controllers/code_controller.js
@@ -1,23 +1,39 @@
 import { Controller } from '@hotwired/stimulus'
-import CodeMirror from 'codemirror'
-import 'codemirror/mode/yaml/yaml'
+import { EditorView, basicSetup } from 'codemirror'
+import { keymap } from '@codemirror/view'
+import { indentLess, indentMore } from '@codemirror/commands'
+import { yaml } from '@codemirror/lang-yaml'
 
 export default class extends Controller {
   connect() {
-    this.editor = new CodeMirror.fromTextArea(this.element, {
-      lineNumbers: true,
-      lineWrapping: true,
-      extraKeys: {
-        'Shift-Tab': 'indentLess',
-        'Tab': 'indentMore',
-      },
-      mode: 'yaml'
+    this.textarea = this.element
+
+    this.view = new EditorView({
+      doc: this.textarea.value,
+      extensions: [
+        basicSetup,
+        yaml(),
+        EditorView.lineWrapping,
+        keymap.of([
+          { key: 'Tab', run: indentMore },
+          { key: 'Shift-Tab', run: indentLess },
+        ]),
+        EditorView.theme({
+          '&': { height: '800px' },
+          '.cm-scroller': { overflow: 'auto' },
+        }),
+        EditorView.updateListener.of((update) => {
+          this.textarea.value = update.state.doc.toString()
+        }),
+      ],
     })
 
-    this.editor.on('blur', () => {
-      this.editor.save()
-    })
+    this.textarea.style.display = 'none'
+    this.textarea.insertAdjacentElement('afterend', this.view.dom)
+  }
 
-    this.editor.setSize(null, 800)
+  disconnect() {
+    this.textarea.style.display = ''
+    this.view?.destroy()
   }
 }
